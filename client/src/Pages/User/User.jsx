05c691452bdfbb2cf534375ffbff669fb2a98a54
SubashD22 <101434737+SubashD22@users.noreply.github.com>
2022-12-13 05:14:50 +0530

    edit user WIP

--- client/src/Pages/User/User.jsx
+++ client/src/Pages/User/User.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React from 'react'
+import { useState } from 'react';
 import { useEffect } from 'react';
 import { useQuery } from 'react-query';
 import { useSelector } from 'react-redux';
@@ -14,6 +15,19 @@ const User = () => {
             navigate('/login')
         }
     }, [user, navigate])
+    const [username, setUsername] = useState({
+        username: '',
+        edit: false
+    });
+    const [email, setEmail] = useState({
+        email: '',
+        edit: false
+    })
+    const [Password, setPassword] = useState({
+        password: '',
+        newPassword: '',
+        changePassword: false
+    })
     return (
         <div className={style.userContainer}>
             <div className={style.userCard}>
@@ -23,8 +37,52 @@ const User = () => {
                     </div>
                 </div>
             </div>
-            <h1>{user?.username}</h1>
-            <p>{user?.email}</p>
+            <div>
+                {username.edit ? <input type='text' name='username' value={username.username} onChange={(e) => {
+                    setUsername({
+                        ...username,
+                        [e.target.name]: e.target.value
+                    })
+                }} />
+                    : <h1>{user?.username}</h1>}
+                {username.edit ? <> <button onClick={() => {
+                    setUsername({
+                        username: '',
+                        edit: false
+                    })
+                }}>Cancel</button>
+                    <button>Submit</button>
+                </> :
+                    <button onClick={() => {
+                        setUsername({
+                            username: user.username,
+                            edit: true
+                        })
+                    }}>Edit</button>}
+            </div>
+            <div>
+                {email.edit ? <input type='email' name='email' value={email.email} onChange={(e) => {
+                    setEmail({
+                        ...email,
+                        [e.target.name]: e.target.value
+                    })
+                }} />
+                    : <p>{user?.email}</p>}
+                {email.edit ? <> <button onClick={() => {
+                    setEmail({
+                        email: '',
+                        edit: false
+                    })
+                }}>Cancel</button>
+                    <button>Submit</button>
+                </> :
+                    <button onClick={() => {
+                        setEmail({
+                            email: user.email,
+                            edit: true
+                        })
+                    }}>Edit</button>}
+            </div>
             <button>Change Password</button>
         </div>
     )
